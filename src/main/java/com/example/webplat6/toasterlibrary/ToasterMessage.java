00af19f85c833bb91aae240f77507d3bb56dc8f9
rajtufan@gmail.com <tdhanbad>
2019-04-10 13:44:46 +0530

    Initial Commit

---
+++ src/main/java/com/example/webplat6/toasterlibrary/ToasterMessage.java
@@ -0,0 +1,15 @@
+package com.example.webplat6.toasterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+  public static void s(Context c, String message){
+
+
+    Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+
+  }
+}
